aa78dde68be275cfed24e7593a6062a77bcfb7f6
liliana ramirez <mlramirez290@misena.edu.co>
2015-09-04 15:16:02 +0200

    ejercicio9 terminada!!

--- desarrollo9.php
+++ desarrollo9.php
@@ -1,34 +1,34 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>ejercicio 9</title>
-    </head>
-    <body>
-        
-    <form method="POST">
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <meta charset="UTF-8">
+    <title>ejercicio 9</title>
+        </head>
+        <body>
 
-    <div class="btn_more">
-    <p><a href="#aboutus">Resultado <span>&raquo;</span></a></p>
-    </div>
+        <form method="POST">
 
-      <?php
-	   if (isset($_POST['txtCosto'])){
-        // put your code here
-      $c=$_POST['txtCosto'];
-	$pt=0;
-	$ini=$c;
-	$suma=$c+300;
-	$por=$c*0.05;
+        <div class="btn_more">
+        <p><a href="#aboutus">Resultado <span>&raquo;</span></a></p>
+        </div>
 
-	$pt=$suma+$por;
-	echo "El precio inicial de la Laptop es: ".$ini;
-	echo "<br>El valor a pagar con los impuestos incluidos es: ".$suma;
-	echo "<br>El valor a pagar a los soldados es: ".$por;
-	echo "<br>El valor final de la Laptop es: ".$pt;
-	
-	   }
-        ?>
-     
-</body>
-</html>
+          <?php
+           if (isset($_POST['txtCosto'])){
+            // put your code here
+          $c=$_POST['txtCosto'];
+        $pt=0;
+        $ini=$c;
+        $suma=$c+300;
+        $por=$c*0.05;
+
+        $pt=$suma+$por;
+        echo "El precio inicial de la Laptop Es: ".$ini;
+        echo "<br>El valor a pagar con los impuestos incluidos es: ".$suma;
+        echo "<br>El valor a pagar a los soldados es: ".$por;
+        echo "<br>El valor final de la Laptop es: ".$pt;
+
+           }
+            ?>
+
+    </body>
+    </html>
